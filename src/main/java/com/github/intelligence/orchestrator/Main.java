64e1849c84e142c8b0ce11c73350fa6cdcc27985
Jesse Sandvik <sandvikjesse@gmail.com>
2023-07-25 19:53:54 -0400

    created own external command dependency containing a command interface and handlers that external commands can implement

--- src/main/java/com/github/intelligence/orchestrator/Main.java
+++ src/main/java/com/github/intelligence/orchestrator/Main.java
@@ -1,14 +1,5 @@
 package com.github.intelligence.orchestrator;
 
-import com.github.intelligence.orchestrator.picocli.IOCommand;
-
-public class Main implements Runnable {
-    static IOCommand ioCommand;
-    public static void main(String[] args) {
-        ioCommand = new IOCommand(args);
-    }
-    @Override
-    public void run() {
-        System.exit(ioCommand.call());
-    }
+public class Main {
+    public static void main(String[] args) {}
 }
