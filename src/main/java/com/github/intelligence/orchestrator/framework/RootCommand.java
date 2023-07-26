17e9f3d4dc405794f00d7a756cd0f6e1feeb7d91
Jesse Sandvik <sandvikjesse@gmail.com>
2023-07-25 22:32:12 -0400

    refactored RootCommand

--- src/main/java/com/github/intelligence/orchestrator/framework/RootCommand.java
+++ src/main/java/com/github/intelligence/orchestrator/framework/RootCommand.java
@@ -1,21 +1,29 @@
 package com.github.intelligence.orchestrator.framework;
 
+import com.command.Command;
+import com.command.CommandFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 import picocli.CommandLine.Model.*;
 
+import java.util.List;
+
 public class RootCommand {
-    static int run(ParseResult pr) {
-        Integer helpExitCode = CommandLine.executeHelpRequest(pr);
-        return helpExitCode != null ? helpExitCode : pr.matchedOptionValue('c', 1);
-    }
-    public static void main(String[] args) {
-        CommandSpec spec = CommandSpec.create()
+    static CommandSpec rootSpec;
+    static List<Command> subcommands;
+    static void setSpec() {
+        rootSpec = CommandSpec.create()
                 .name("io")
                 .version("[IO] Version 1.0")
                 .mixinStandardHelpOptions(true);
-
-        spec.usageMessage()
+    }
+    static void setSubcommands() {
+        CommandFactory subcommandsFactory = new CommandFactory();
+//        subcommandsFactory.addcommand();
+        subcommands = subcommandsFactory.commands;
+    }
+    static void setUsageMessage() {
+        rootSpec.usageMessage()
                 .headerHeading("Header heading%n")
                 .header("header line 1", "header line 2")
                 .descriptionHeading("Description heading%n")
@@ -24,10 +32,19 @@ public class RootCommand {
                 .parameterListHeading("Positional Parameters%n")
                 .footerHeading("Footer heading%n")
                 .footer("footer line 1", "footer line 2");
+    }
+    static int run(ParseResult pr) {
+        Integer helpExitCode = CommandLine.executeHelpRequest(pr);
+        return helpExitCode != null ? helpExitCode : pr.matchedOptionValue('c', 1);
+    }
+    public static void main(String[] args) {
+        setSpec();
+        setSubcommands();
+        setUsageMessage();
 
-        CommandLine cmd = new CommandLine(spec);
-
+        CommandLine cmd = new CommandLine(rootSpec);
         cmd.setExecutionStrategy(RootCommand::run);
+
         int exitCode = cmd.execute(args);
         System.exit(exitCode);
     }
