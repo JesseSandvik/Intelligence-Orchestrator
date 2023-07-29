e5f7ba747ec9f66b73f6b47d0e76faabfa5dd295
Jesse <sandvikjesse@gmail.com>
2023-07-28 22:19:48 -0400

    working on FileSystem, system layer

--- src/main/java/com/github/intelligence/orchestrator/system/FileSystem.java
+++ src/main/java/com/github/intelligence/orchestrator/system/FileSystem.java
@@ -1,11 +1,21 @@
 package com.github.intelligence.orchestrator.system;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public class FileSystem {
     private static File file;
     private static String fileName;
+    public void createFile(String fileNameArg) throws IOException {
+        File fileToCreate = new File(fileNameArg);
+
+        if (fileToCreate.createNewFile()) {
+            System.out.println("File created: " + fileNameArg);
+        } else {
+            System.out.println("File already exists: " + fileNameArg);
+        }
+    }
     public boolean fileExists(String fileNameArg) {
         if (file == null || !Objects.equals(fileNameArg, fileName)) {
             file = new File(fileNameArg);
@@ -13,7 +23,7 @@ public class FileSystem {
         }
         return file.exists();
     }
-    public boolean fileCanExecute(String fileNameArg) {
+    public boolean fileExistsAndExecutes(String fileNameArg) {
         if (file == null || !Objects.equals(fileNameArg, fileName)) {
             file = new File(fileNameArg);
             fileName = fileNameArg;
