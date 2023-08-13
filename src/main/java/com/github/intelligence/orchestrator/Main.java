e1b08016f70cd5af5aebf9d76458da43e1de7941
Jesse <sandvikjesse@gmail.com>
2023-08-13 19:02:12 -0400

    implemented EchoCommand into project Main, behaving as expected.

--- src/main/java/com/github/intelligence/orchestrator/Main.java
+++ src/main/java/com/github/intelligence/orchestrator/Main.java
@@ -1,9 +1,11 @@
 package com.github.intelligence.orchestrator;
 
+import com.github.intelligence.orchestrator.command.EchoCommand;
 import com.github.intelligence.orchestrator.picocli.PicocliService;
 import com.github.intelligence.orchestrator.properties.PropertiesService;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
@@ -12,7 +14,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        PropertiesService rootCommandPropertiesService = new PropertiesService("/rootcommand.properties");
+        PropertiesService rootCommandPropertiesService = new PropertiesService("/io.properties");
         String rootCommand = rootCommandPropertiesService.getProperty("app.command");
         String rootCommandVersion = rootCommandPropertiesService.getProperty("app.version");
         String applicationName = rootCommandPropertiesService.getProperty("app.name");
@@ -26,18 +28,20 @@ public class Main {
                 applicationDescription
         );
 
-        PropertiesService subcommandPropertiesService = new PropertiesService("/subcommand.properties");
+        PropertiesService subcommandPropertiesService = new PropertiesService("/echo.properties");
         String subcommand = subcommandPropertiesService.getProperty("subcommand.command");
         String subcommandVersion = subcommandPropertiesService.getProperty("subcommand.version");
         String subcommandDescription = subcommandPropertiesService.getProperty("subcommand.description");
 
         String formattedSubcommandVersion = getFormattedVersion(subcommand.toUpperCase(), subcommandVersion);
 
+        String echoCommandArgument = args.length == 2 && Objects.equals(args[0], "echo") ? args[1] : "";
+
         cliService.addSubcommand(
                 subcommand,
                 formattedSubcommandVersion,
                 subcommandDescription,
-                () -> {}
+                new EchoCommand(echoCommandArgument)
         );
         String formattedSubcommandFirstParameter = "[TEXT]";
         cliService.addParameterForSubcommand(subcommand, formattedSubcommandFirstParameter, String.class, "The text to be displayed as an output.");
